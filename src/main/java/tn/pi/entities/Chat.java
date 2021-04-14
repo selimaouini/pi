f1a0c45f137deba1cb4e4c970a82563a293a9ddf
Nahawand Laajili <nahawand.laajili@esprit.tn git congig --global user.name Nahawand>
2021-04-14 03:00:01 +0100

    Nahawand

---
+++ src/main/java/tn/pi/entities/Chat.java
@@ -0,0 +1,68 @@
+package tn.pi.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_CHAT")
+public class Chat implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idch;
+	
+	@Column(name="message")
+	private String message;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	User User ;
+	
+	public long getIdch() {
+		return idch;
+	}
+
+	public void setIdch(long idch) {
+		this.idch = idch;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Chat [idch=" + idch + ", message=" + message + "]";
+	}
+
+	public Chat(long idch, String message) {
+		super();
+		this.idch = idch;
+		this.message = message;
+	}
+
+	public Chat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
+
+
